8261ba80f6c22aca3da6a5ec7d375fa171232b01
Jeremie Rossignol <jrossignol@gmail.com>
2015-06-25 16:04:30 -0600

    Work in process.

---
+++ Param/sidebar.pl
@@ -0,0 +1,188 @@
+#!/usr/bin/env perl
+
+use strict;
+use File::Copy qw(move);
+
+sub MakeName($)
+{
+    my $name = shift;
+
+    # Get the tag
+    my $tag = $name;
+    $tag =~ s/\.md$//g;
+    $tag =~ s/ *Parameter$//g;
+    $tag =~ s/ *Requirement$//g;
+    $tag =~ s/ *Behaviour$//g;
+    $tag =~ s/[\/\(\)]//g;
+
+    return $tag;
+}
+
+sub MakeTag($)
+{
+    my $name = shift;
+
+    # Get the tag
+    my $tag = $name;
+    $tag =~ s/\.md$//g;
+    $tag =~ s/ /-/g;
+    $tag =~ s/[\/\(\)]//g;
+
+    return $tag;
+}
+
+sub cleanFileName($)
+{
+    my $fname = shift;
+    $fname =~ s/^\d+-//g;
+    return $fname;
+}
+
+sub handleDir($$)
+{
+    my $dir = shift;
+    my $prefix = shift;
+
+    my @prefixes = split /$;/, $prefix;
+    my $count = scalar(@prefixes);
+    my $spaces = ($count-1) * 4;
+    if ($count == 1)
+    {
+        print "**$prefixes[-1]**\n"
+    }
+    else
+    {
+        print ((' ' x ($spaces-4)), "* ", $prefixes[-1], "\n");
+    }
+    
+    my $dh;
+    opendir $dh, $dir or die "cannot open $dir: $!";
+    my @sorted_dir = 
+        map $_->[0],
+        sort {
+            $a->[1] <=> $b->[1] || $a->[0] cmp $b->[0]
+        }
+        map [ $_, -f "$dir/$_" ],
+        readdir($dh);
+
+    foreach my $file (@sorted_dir)
+    {
+        if ($file eq '.' || $file eq '..' || $file =~ /^_/)
+        {
+            next;
+        }
+        elsif (-d "$dir/$file")
+        {
+            handleDir("$dir/$file", "$prefix$;" . cleanFileName($file));
+        }
+        elsif ($file =~ /\.md$/)
+        {
+            my $name = MakeName($file);
+            my $tag = MakeTag($file);
+
+            print((' ' x $spaces) . "* [[$name|$tag]]\n");
+        }
+    }
+    closedir $dh; 
+}
+
+handleDir(".", "Parameters");
+exit 1;
+
+sub stuff {
+foreach my $file (@ARGV)
+{
+    my @lines;
+
+    my $minCount = 10;
+    my $countAtLevel = 0;
+
+    # First pass to look for the headings
+    open IFILE, "<$file";
+    my $line;
+    while ($line = <IFILE>)
+    {
+        if ($line =~ /^#/)
+        {
+            my $count = () = $line =~ /#/g;
+
+            # Logic for determining minCount
+            if ($count < $minCount)
+            {
+                $minCount = $count if $count < $minCount;
+            }
+            if ($count == $minCount)
+            {
+                $countAtLevel++;
+            }
+
+            push @lines, $line;
+        }
+    }
+    close IFILE;
+
+    if ($countAtLevel <= 1)
+    {
+        $minCount += 1;
+    }
+
+    # Open in and out files
+    open IFILE, "<$file";
+    open OFILE, ">out.txt";
+
+    # FFWD to the correct location
+    while ($line = <IFILE>)
+    {
+        if ($line !~ /^\* \[\[/)
+        {
+            print OFILE $line;
+        }
+        else
+        {
+            last;
+        }
+    }
+
+    # Write new TOC
+    my $identifier = $file;
+    $identifier =~ s/\.md$//;
+    foreach my $line (@lines)
+    {
+        # How many spaces to prepend
+        my $count = () = $line =~ /#/g;
+        my $spaces = ($count - $minCount) * 2;
+
+        if ($spaces < 0)
+        {
+            next;
+        }
+
+        # Get the name & tag
+        my $name = Util::MakeName($line);
+        my $tag = Util::MakeTag($line);
+
+        print(OFILE (' ' x $spaces) . "* [[$name|$identifier#$tag]]\n");
+    }
+
+    # Dump the garbage
+    while ($line = <IFILE>)
+    {
+        if ($line !~ /^ *\* \[\[/)
+        {
+            print OFILE $line;
+            last;
+        }
+    }
+
+    # Write the remainder
+    while ($line = <IFILE>)
+    {
+        print OFILE $line;
+    }
+
+    close IFILE;
+    close OFILE;
+
+    move("out.txt", $file);
+}
+}
