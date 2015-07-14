7900799d9dd6560298b6ccbdfb619e42ecdc1564
Jeremie Rossignol <jrossignol@gmail.com>
2015-07-14 08:48:53 -0600

    Added types.pl

---
+++ types.pl
@@ -0,0 +1,172 @@
+#!/usr/bin/env perl
+
+use strict;
+use File::Copy qw(move);
+
+my %IGNORED_TYPES = (
+    Boolean => 1,
+    Enumeration => 1,
+    List => 1,
+    Numeric => 1,
+    String => 1,
+);
+
+my %TYPE_MAP = qw(
+    bool Boolean-Type
+    string String-Type
+    uint Numeric-Type
+    int Numeric-Type
+    long Numeric-Type
+    float Numeric-Type
+    double Numeric-Type
+    BodyLocation Enumeration-Type
+    ExperimentSituations Enumeration-Type
+    HasAntennaParameter.AntennaType Enumeration-Type
+    PartCategories Enumeration-Type
+    SCANdata.SCANtype Enumeration-Type
+    ScienceRecoveryMethod Enumeration-Type
+    Timer.TimerType Enumeration-Type
+    Vessel.Situations Enumeration-Type
+    VesselType Enumeration-Type
+);
+
+my %unhandledTypes;
+
+
+sub MakeName($)
+{
+    my $name = shift;
+
+    # Get the tag
+    my $tag = $name;
+    $tag =~ s/\.md$//g;
+    $tag =~ s/-/ /g;
+    $tag =~ s/ (Parameter|Requirement|Behaviour|Type)$//g;
+    $tag =~ s/[\/\(\)]//g;
+
+    return $tag;
+}
+
+sub MakeTag($)
+{
+    my $name = shift;
+
+    # Get the tag
+    my $tag = $name;
+    $tag =~ s/\.md$//g;
+    $tag =~ s/ /-/g;
+    $tag =~ s/[\/\(\)]//g;
+
+    return $tag;
+}
+
+sub HandleDir($)
+{
+    my $dir = shift;
+
+    my $dh;
+    opendir $dh, $dir or die "cannot open $dir: $!";
+
+    foreach my $file (readdir($dh))
+    {
+        if ($file =~ /^[\._]/ ||
+            $file eq 'Expressions.md' ||
+            $file eq 'Global-Functions.md' ||
+            $file =~ /-Behaviour/)
+        {
+            next;
+        }
+        elsif (-d "$dir/$file")
+        {
+            HandleDir("$dir/$file");
+        }
+        elsif ($file =~ /\.md$/)
+        {
+            HandleFile("$dir/$file");
+        }
+    }
+    closedir $dh; 
+}
+
+sub HandleFile($)
+{
+    my $file = shift;
+
+    open IFILE, "<$file" or die "Couldn't open input file $file!: $!";
+    open OFILE, ">out.txt" or die "Couldn't open input file $file!: $!";
+
+    while (my $line = <IFILE>)
+    {
+        $line =~ s/[\n\r]//g;
+
+        if ($line =~ /Type: +([^< ].*)/)
+        {
+            my $type = $1;
+            if (exists $TYPE_MAP{$type})
+            {
+                $line =~ s/$type/<a href="$TYPE_MAP{$type}">$type<\/a>/;
+            }
+            else
+            {
+                $unhandledTypes{$type} = 1;
+            }
+        }
+
+        print OFILE $line, "\n";
+    }
+
+    close IFILE;
+    close OFILE;
+
+    move("out.txt", $file);
+}
+
+sub LoadTypes($)
+{
+    my $dir = shift;
+
+    my $dh;
+    opendir $dh, $dir or die "cannot open $dir: $!";
+
+    foreach my $file (readdir($dh))
+    {
+        if ($file =~ /^[\._]/ ||
+            $file eq 'Expressions.md' ||
+            $file eq 'Global-Functions.md' ||
+            $file =~ /-Behaviour/)
+        {
+            next;
+        }
+        elsif (-d "$dir/$file")
+        {
+            LoadTypes("$dir/$file");
+        }
+        else
+        {
+            my $type = MakeName($file);
+
+            if (exists $IGNORED_TYPES{$type})
+            {
+                next;
+            }
+            my $tag = MakeTag($file);
+
+            $TYPE_MAP{$type} = $tag;
+        }
+    }
+
+    closedir $dh;
+}
+
+
+LoadTypes("5-Expressions");
+
+foreach my $rootDir (@ARGV)
+{
+    HandleDir($rootDir);
+}
+
+foreach my $type (sort keys %unhandledTypes)
+{
+    print "Unhandled type: $type\n";
+}
