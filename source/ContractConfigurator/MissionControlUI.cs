dcdd26c0be1fc384014a6fdb07aeaef329ffdd76
yadenisyur <33399239+yadenisyur@users.noreply.github.com>
2018-11-30 18:15:03 +0700

    Add files via upload

--- source/ContractConfigurator/MissionControlUI.cs
+++ source/ContractConfigurator/MissionControlUI.cs
@@ -1017,7 +1017,8 @@ namespace ContractConfigurator.Util
             // Setup the available text
             TMPro.TextMeshProUGUI availableText = availableTextObject.GetComponent<TMPro.TextMeshProUGUI>();
             availableText.alignment = TMPro.TextAlignmentOptions.BottomRight;
-            availableText.text = "<color=#" + (groupContainer.availableContracts == 0 ? "CCCCCC" : "8BED8B") + Localizer.Format("#autoLOC_ContractConfigurator_010")/*>Offered: */ + groupContainer.availableContracts + "</color>";
+            string Offered =  Localizer.Format("#autoLOC_ContractConfigurator_010")/*>Offered: */;
+            availableText.text = "<color=#" + (groupContainer.availableContracts == 0 ? "CCCCCC" : "8BED8B") + Offered + groupContainer.availableContracts + "</color>";
             availableText.fontSize = groupContainer.mcListItem.title.fontSize - 3;
 
             // Setup the group text
